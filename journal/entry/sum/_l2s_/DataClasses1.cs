176d06be1169295c4e407c5920314ffde0ec5d03
WANG,Youtian <wangyoutian@nilnul.com>
2023-09-27 11:52:30 +0800

    connect to db: journal/entry/sum/_l2s_/DataClasses1.cs; etc;

---
+++ journal/entry/sum/_l2s_/DataClasses1.cs
@@ -0,0 +1,11 @@
+namespace nilnul._finance_._LINQ_.journal.entry.sum._l2s_
+{
+	partial class DataClasses1DataContext
+	{
+		static public DataClasses1DataContext Ov() {
+			return new DataClasses1DataContext(
+				_nilnul.cfg.linq_.defaulted_._AppX.OnAppCfg()
+			);
+		}
+	}
+}
